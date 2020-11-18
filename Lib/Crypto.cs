d20eb8629f866fc93bf4274b6ec638a7cb116184
SammyBM <samuelbarraganmiranda@gmail.com>
2020-11-18 12:18:19 -0600

    Final

---
+++ Lib/Crypto.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+using System.Xml.Linq;
+using static System.Convert;
+
+namespace Lib
+{
+    public static class Protector
+    {
+        //Generate key tupple and export all information as parameters.
+        private static RSAParameters Keys = new RSACryptoServiceProvider().ExportParameters(true);
+
+        #region RSA
+
+        public static string xmlToString() //Converts XML formatted text into a non formatted string.
+        {
+            using (RSA rsa = new RSACryptoServiceProvider())
+            {
+                rsa.ImportParameters(Keys);
+                return rsa.ToXmlString(false);
+            }
+        }
+    
+    
+        public static void stringToXml(string xml) //Converts a non formatted string into XML format.
+        {
+            using (RSA rsa = new RSACryptoServiceProvider())
+            {
+                rsa.FromXmlString(xml);
+                Keys = rsa.ExportParameters(false);
+            }
+        }
+        
+        
+        public static string generateSign(string text) //Generates a signature based on the key tupple generated before.
+        {
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider()) //Using disposes of the object in parenthesis after the brackets are closed.
+            {
+                rsa.ImportParameters(Keys);
+
+                byte[] signatureBytes = rsa.SignData(Encoding.Unicode.GetBytes(text), SHA256.Create());
+
+                //Pase to a Base64 formatted string and return it
+                return ToBase64String(signatureBytes);
+            }
+        }
+        
+        
+        public static bool validateSign(string text, string sign) //If the signature is valid returns true, otherwise returns false.
+        {
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                //Config the provider with the current key
+                rsa.ImportParameters(Keys);
+
+                byte[] signatureBytes = FromBase64String(sign);
+                byte[] messageBytes = Encoding.Unicode.GetBytes(text);
+
+                //Return result
+                return rsa.VerifyData(messageBytes, SHA256.Create(), signatureBytes);
+            }
+        }
+        #endregion
+    }
+}
---
+++ Main/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using static System.Console;
+using Lib;
+using System.Security.Cryptography;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Please enter your text: ");
+            string message = ReadLine();
+
+            //Creates a real signature and a fake one, therefore the parameters are different.
+            string signature = Protector.generateSign(message);
+            string xmlText = Protector.xmlToString();
+            string fakeSignature = signature.Replace(signature[0], signature[1]);
+
+            //Prints results.
+            WriteLine($"\n XML String: {xmlText} \n");
+
+            WriteLine($"\n Signature: {signature} \n");
+            validate(message, signature);
+
+            WriteLine($"\n Signature: {fakeSignature} \n");
+            validate(message, fakeSignature);
+        }
+
+        static public void validate(string text, string sign) //Takes a bool to return a readable output.
+        {
+            if (Protector.validateSign(text, sign))  //If the signature is correct, therefore "if true".
+            {
+                WriteLine("Correct signature"); 
+            }
+            else                                      //If the sirgnature is not correct.
+            {
+                WriteLine("Wrong signature");
+            }
+        }
+    }
+}
